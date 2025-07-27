e4b2325d22394ed2d646783ea612ec1b2e64635e
Tolu Daniel Ojo <166349763+toluenensama@users.noreply.github.com>
2025-07-28 00:31:15 +0100

    Update admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -4,24 +4,35 @@ from .models import Photo
 
 # Register your models here.
 
+# @admin.register(Photo)
+# class PhotoAdmin(admin.ModelAdmin):
+#     """Customizes the display of the Photo model in the Django admin."""
+#     list_display = ('image_thumbnail', 'title', 'short_description', 'uploaded_at')
+#     list_filter = ('uploaded_at',)
+#     search_fields = ('title', 'description')
+#     readonly_fields = ('image_thumbnail',)
+
+#     def image_thumbnail(self, obj):
+#         """Creates a thumbnail of the image to display in the admin list."""
+#         if obj.image:
+#             return mark_safe(f'<img src="{obj.image.url}" width="100" />')
+#         return "—"  # Display a dash if no image is present
+#     image_thumbnail.short_description = 'Thumbnail'
+
+#     def short_description(self, obj):
+#         """Truncates the description for a cleaner list view."""
+#         if obj.description and len(obj.description) > 75:
+#             return obj.description[:75] + '...'
+#         return obj.description or "—"
+#     short_description.short_description = 'Description'
+
+from django.utils.html import format_html
+
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    """Customizes the display of the Photo model in the Django admin."""
-    list_display = ('image_thumbnail', 'title', 'short_description', 'uploaded_at')
-    list_filter = ('uploaded_at',)
-    search_fields = ('title', 'description')
-    readonly_fields = ('image_thumbnail',)
+    list_display = ('id', 'title', 'uploaded_at', 'thumbnail')
 
-    def image_thumbnail(self, obj):
-        """Creates a thumbnail of the image to display in the admin list."""
+    def thumbnail(self, obj):
         if obj.image:
-            return mark_safe(f'<img src="{obj.image.url}" width="100" />')
-        return "—"  # Display a dash if no image is present
-    image_thumbnail.short_description = 'Thumbnail'
-
-    def short_description(self, obj):
-        """Truncates the description for a cleaner list view."""
-        if obj.description and len(obj.description) > 75:
-            return obj.description[:75] + '...'
-        return obj.description or "—"
-    short_description.short_description = 'Description'
+            return format_html('<img src="{}" width="100" />', obj.image.url)
+        return "-"
