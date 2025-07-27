08b883a20a3833d602fca56f2ffef8623b51276c
Tolu Daniel Ojo <166349763+toluenensama@users.noreply.github.com>
2025-07-28 00:09:45 +0100

    Update models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class Photo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='photos/')
+    image = CloudinaryField('image')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
